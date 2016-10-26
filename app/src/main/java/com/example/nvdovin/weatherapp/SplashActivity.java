53f95b38c2383fe264a5dc8bb50553a21e5fa56d
endavainterns2016 <endava.interns2016@gmail.com>
2016-10-26 14:05:43 +0300

    Added SplashScreen logic (Edited theme, deleted ~created by~)

--- app/src/main/java/com/example/nvdovin/weatherapp/SplashActivity.java
+++ app/src/main/java/com/example/nvdovin/weatherapp/SplashActivity.java
@@ -4,10 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by mcebotari on 10/26/2016.
- */
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
