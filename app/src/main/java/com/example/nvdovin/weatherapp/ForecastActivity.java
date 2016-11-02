201740af13cec2bdf337d40164213b3a01821eb8
MaxTheWebDev <endava.interns2016@gmail.com>
2016-11-02 11:20:18 +0200

    US4 - Fetch DU List once a day, update weather Information not more often than 24h

--- app/src/main/java/com/example/nvdovin/weatherapp/ForecastActivity.java
+++ app/src/main/java/com/example/nvdovin/weatherapp/ForecastActivity.java
@@ -2,26 +2,33 @@ package com.example.nvdovin.weatherapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.nvdovin.weatherapp.adapter.RecyclerViewAdapter;
 import com.example.nvdovin.weatherapp.adapter.SeparatorDecoration;
 import com.example.nvdovin.weatherapp.database.model.City;
 import com.example.nvdovin.weatherapp.factory.GreenDaoFactory;
 import com.example.nvdovin.weatherapp.factory.RetrofitFactory;
-import com.example.nvdovin.weatherapp.utils.Constants;
+import com.example.nvdovin.weatherapp.utils.SharedPrefs;
+import com.example.nvdovin.weatherapp.utils.TemperatureScales;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ForecastActivity extends AppCompatActivity implements ForecastView {
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ForecastPresenter forecastPresenter;
     private ProgressBar progressBar;
     private RecyclerViewAdapter recycleViewAdapter;
+    private GreenDaoFactory greenDaoFactory;
+    private SharedPrefs sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +37,35 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         progressBar = (ProgressBar) findViewById(R.id.forecast_progress_bar);
 
-        Constants constants = new Constants();
-        int CELSIUS_SCALE = constants.getCelsiusScale();
+
         List<City> cityList = new ArrayList<City>();
-
         TypedValue outValue = new TypedValue();
-
         getResources().getValue(R.dimen.separator_height, outValue, true);
+
+        sharedPrefs = new SharedPrefs(this);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.forecast_swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshProcedure();
+            }
+        });
 
         float separatorHeight = outValue.getFloat();
         SeparatorDecoration separatorDecoration = new SeparatorDecoration(this, Color.GRAY, separatorHeight);
+
         RetrofitFactory retrofitFactory = new RetrofitFactory();
-        GreenDaoFactory greenDaoFactory = new GreenDaoFactory(this);
+        greenDaoFactory = new GreenDaoFactory(this);
 
         ForecastPresenter forecastPresenter = new ForecastPresenter(retrofitFactory, greenDaoFactory, this);
-        forecastPresenter.getData();
+        this.forecastPresenter = forecastPresenter;
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(separatorDecoration);
-        recycleViewAdapter = new RecyclerViewAdapter(cityList, CELSIUS_SCALE, this);
+        recycleViewAdapter = new RecyclerViewAdapter(cityList, TemperatureScales.getCelsiusScale(), this);
         recyclerView.setAdapter(recycleViewAdapter);
-
+        checkLastUpdateTime();
     }
 
     @Override
@@ -62,5 +77,23 @@ public class ForecastActivity extends AppCompatActivity implements ForecastView 
     @Override
     public void hideLoading() {
         progressBar.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    public void refreshProcedure() {
+        Toast.makeText(getApplicationContext(), "Refreshing data...", Toast.LENGTH_SHORT).show();
+        swipeRefreshLayout.setRefreshing(true);
+        sharedPrefs.setLastUpdateTime();
+        forecastPresenter.getData();
+
+    }
+
+    public void checkLastUpdateTime() {
+        if (sharedPrefs.lastUpdateExceededLimit()){
+            forecastPresenter.getData();
+        } else {
+            displayData(greenDaoFactory.loadCities());
+            hideLoading();
+        }
     }
 }
---
+++ app/src/main/java/com/example/nvdovin/weatherapp/utils/TemperatureScales.java
@@ -0,0 +1,7 @@
+package com.example.nvdovin.weatherapp.utils;
+
+public class TemperatureScales {
+    public static int getCelsiusScale(){
+        return 273;
+    }
+}
