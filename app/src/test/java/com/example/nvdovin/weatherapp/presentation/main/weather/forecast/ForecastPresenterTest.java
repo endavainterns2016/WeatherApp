10bd8f0cadfaba30e2f37f159bab855af528b4f1
VdovinN <VdovinN@users.noreply.github.com>
2016-12-08 10:48:50 +0200

    Testing Forecast presenter (#51)

---
+++ app/src/test/java/com/example/nvdovin/weatherapp/presentation/main/weather/forecast/ForecastPresenterTest.java
@@ -0,0 +1,116 @@
+package com.example.nvdovin.weatherapp.presentation.main.weather.forecast;
+
+import com.example.nvdovin.weatherapp.data.SortQueryBuilder;
+import com.example.nvdovin.weatherapp.data.model.City;
+import com.example.nvdovin.weatherapp.domain.model.CityForecast;
+import com.example.nvdovin.weatherapp.domain.service.CityService;
+import com.example.nvdovin.weatherapp.domain.utils.eventbus.EventBusWrapper;
+import com.example.nvdovin.weatherapp.domain.utils.executor.DefaultThreadPoolExecutor;
+import com.example.nvdovin.weatherapp.domain.utils.executor.Executor;
+import com.example.nvdovin.weatherapp.domain.utils.mapper.DataMapper;
+import com.example.nvdovin.weatherapp.domain.utils.sharedpreferences.SharedPrefs;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ForecastPresenterTest {
+
+    private ForecastPresenter forecastPresenter;
+
+    @Mock
+    private ForecastView view;
+    @Mock
+    private SharedPrefs sharedPrefs;
+    @Mock
+    private CityService cityService;
+    @Mock
+    private Executor executor;
+    @Mock
+    private DataMapper dataMapper;
+    @Mock
+    private DefaultThreadPoolExecutor defaultThreadPoolExecutor;
+    @Mock
+    private SortQueryBuilder sortQueryBuilder;
+    @Mock
+    private EventBusWrapper eventBusWrapper;
+
+    @Before
+    public void setUp() throws Exception {
+        forecastPresenter = new ForecastPresenter(
+                executor,
+                cityService,
+                view,
+                sharedPrefs,
+                dataMapper,
+                defaultThreadPoolExecutor,
+                sortQueryBuilder,
+                eventBusWrapper
+        );
+
+    }
+
+    @Test
+    public void testSetData() throws Exception {
+
+        forecastPresenter.setData(ArgumentMatchers.<CityForecast>anyList());
+
+        verify(view).displayData(ArgumentMatchers.<CityForecast>anyList());
+        verify(view).setRefreshing(false);
+
+    }
+
+    @Test
+    public void testRefreshCalled() throws Exception {
+
+        forecastPresenter.refreshCalled();
+
+        verify(sharedPrefs).setLastUpdateTime();
+        verify(defaultThreadPoolExecutor).executeBackground(executor);
+
+    }
+
+    @Test
+    public void testWhenLastUpdateExceedLimit() throws Exception {
+
+        doReturn(true).when(sharedPrefs).lastUpdateExceededLimit();
+
+        forecastPresenter.checkLastUpdateTime();
+
+        verify(sharedPrefs).lastUpdateExceededLimit();
+        verify(defaultThreadPoolExecutor).executeBackground(executor);
+        verify(view).hideLoading();
+
+    }
+
+
+    @Test
+    public void testWhenLastUpdateNotExceedLimit() throws Exception {
+
+        List<City> mockCities = mock(List.class);
+
+        doReturn(false).when(sharedPrefs).lastUpdateExceededLimit();
+
+        when(cityService.loadSortedCities(sortQueryBuilder)).thenReturn(mockCities);
+
+        forecastPresenter.checkLastUpdateTime();
+
+        verify(cityService).loadSortedCities(sortQueryBuilder);
+        verify(view).displayData(ArgumentMatchers.<CityForecast>anyList());
+        verify(view).setRefreshing(false);
+        verify(view).hideLoading();
+
+    }
+
+}
